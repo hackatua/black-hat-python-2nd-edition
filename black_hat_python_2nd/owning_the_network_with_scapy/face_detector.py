5988a6116c98fcfcab57eeac4c75a943dd938647
Hackatua <hackatua@gmail.com>
2023-01-03 16:57:13 +0100

    Adds a face detector scritp

---
+++ black_hat_python_2nd/owning_the_network_with_scapy/face_detector.py
@@ -0,0 +1,47 @@
+import cv2
+import os
+import sys
+
+# apt-get install libopencv-dev python3-opencv python3-numpy python3-scipy
+# wget http://eclecti.cc/files/2008/03/haarcascade_frontalface_alt.xml
+
+ROOT = '.'
+FACES = '.'
+TRAIN_XML = 'haarcascade_frontalface_alt.xml'
+
+def detect(src_dir=ROOT, tgt_dir=FACES, train_xml=TRAIN_XML):
+    for fname in os.listdir(src_dir):
+        if not fname.upper().endswith('.JPG'):
+            continue
+        fullname = os.path.join(src_dir, fname)
+        newname = os.path.join(tgt_dir, f'faces_{fname}')
+
+        img = cv2.imread(fullname)
+        if img is None:
+            continue
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        training = os.path.join(train_xml)
+        cascade = cv2.CascadeClassifier(training)
+        rects = cascade.detectMultiscale(gray, 1.3, 5)
+        try:
+            if rects.any:
+                print('Got a face!')
+                rects[:, 2:] += rects[:, :2]
+        except AttributeError:
+            print(f'No faces found in {fname}.')
+            continue
+
+        for x1, y1, x2, y2 in rects:
+            cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
+
+        cv2.imwrite(newname, img)
+
+def main():
+    (src_dir, tgt_dir, train_xml) = (
+        sys.argv[1], sys.orig_argv[2], sys.argv[3]
+    )
+    detect(src_dir, tgt_dir, train_xml)
+
+if __name__ == '__main__':
+    main()
